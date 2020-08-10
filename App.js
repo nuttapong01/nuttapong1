b7b7683284a35545088384ab136f7913dc935dad
nuttapong01 <69332738+nuttapong01@users.noreply.github.com>
2020-08-09 19:51:44 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function App(){
+  return(
+    <View Style={{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+      }}>
+      <Text>Hello Word</Text>
+    </View>
+  )
+}
